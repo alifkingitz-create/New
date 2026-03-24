3d4a42c3a9422bdbf838bcd2d659787fd17a73e0
alifkingitz-create <alifkingitz@gmail.com>
2026-03-24 15:41:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const music = document.getElementById("bg-music");
+const btn = document.getElementById("music-btn");
+
+/* first click e music start */
+document.body.addEventListener("click", () => {
+  music.play();
+}, {once:true});
+
+btn.onclick = () => {
+  if(music.paused) music.play();
+  else music.pause();
+};
+
+/* navigation */
+function go(page){
+  window.location.href=page;
+}
+
+function back(){
+  window.history.back();
+}
+
+/* letters */
+function openLetter(n){
+  let img=["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"];
+  let text=[
+    "Tumi amar sobcheye special 💖",
+    "Miss korle amar kache chole asho 🥺",
+    "Tomar smile amar duniya 😍",
+    "Rag korle amake bolo 😘",
+    "Ami sobsomoy tomar sathe 💕"
+  ];
+
+  document.getElementById("popup").style.display="block";
+  document.getElementById("photo").src=img[n-1];
+  document.getElementById("line").innerText=text[n-1];
+}
+
+function closePopup(){
+  document.getElementById("popup").style.display="none";
+}
+
+/* flower rain */
+function createFlower(){
+  const f=document.createElement("span");
+  f.innerHTML="🌸";
+  f.style.left=Math.random()*100+"vw";
+  f.style.animationDuration=(Math.random()*3+2)+"s";
+  document.querySelector(".flowers")?.appendChild(f);
+  setTimeout(()=>f.remove(),5000);
+}
+
+setInterval(createFlower,300);
